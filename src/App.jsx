eb284c03f2e7aafafc048b36a7e925f737cc1fc2
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-06-04 01:32:15 +0530

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -3,70 +3,67 @@ import {
   Card,
   CardHeader,
 } from "@/components/ui/card"
+import './App.css'
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import axios from 'axios'
-import { WeatherCard } from './WeatherCard'
+import {WeatherCard} from './WeatherCard'
+
 
 
 const App = () => {
-  const [isLoading , setIsLoading] = useState(null);
-  const [weatherData , setWeatherData] = useState(null);
-  const [search , setSearch] = useState('')
+  const [searchValue, setSearchValue] = useState("")
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  const getSearch = async () => {
-    setIsLoading(true)
+  const getWeatherData = async () => {
+    setLoading(true);
     try {
-      const res = await axios.get(`https://api.weatherapi.com/v1/current.json`, {
+      const data = await axios.get(`https://api.weatherapi.com/v1/current.json`, {
         params : {
-          key: "a5a55c53d5794174b7e93927232712",
-          q: search,
+          key: "62e62201d622448ab9a110041242903",
+          q: searchValue,
         }
       });
-
-      setWeatherData(res.data)
+      setWeatherData(data.data); 
     } catch (error) {
       console.log(error.message);
       alert("Failed to fetch weather data");
-    }finally{
-      setIsLoading(false)
+    } finally {
+      setLoading(false);
     }
-  }
+  }   
 
-  const onSubmit = () => {
-    getSearch()
-  }
+  // useEffect(() => {
+  //   if(searchValue){
+  //     getWeatherData();
+  //   }
+  // }, [searchValue]);
 
+  const handleSearch = () => {
+    getWeatherData();
+  }
 
   return (
-    <section className='h-[100vh]'>
-      <div className='text-center  h-[40%] flex items-center justify-center'>
-        <Card className = "w-[25rem]">
-          <CardHeader >
-          <div className = "flex items-center gap-4 " >
-            <Input
-              type = 'text'
-              placeholder = "Enter city name" 
-              className = " text-[1.2rem] placeholder:text-stone-400"
-              value = {search}
-              onChange = {(e) => setSearch(e.target.value)}
+    <>
+      <div className='app'>
+          <div className='search-div'>
+            <input 
+              type="text"
+              className='input'
+              value={searchValue} 
+              placeholder='Enter city name'
+              onChange={(e)=> setSearchValue(e.target.value)}
             />
-            <Button 
-              variant = "green" 
-              onClick = {onSubmit}
-            >Search</Button>
+            <button className='search-btn' onClick={handleSearch}>Search</button>
           </div>
-          </CardHeader>
-        </Card>
+          {loading && <p>Loading data...</p>}
+          {weatherData ? (
+            <WeatherCard data={weatherData} />
+          ) : ""}
       </div>
-      {isLoading && <p className='text-center mb-5'>Loading data...</p>}
-      {!weatherData ? (
-        ""
-      ) : (
-        <WeatherCard data = {weatherData} />
-      )}
-    </section>
+    </>
   )
 }
 
--- src/WeatherCard.jsx
+++ src/WeatherCard.jsx
@@ -1,36 +1,26 @@
-import React from 'react'
-import { Card } from './components/ui/card'
 
-export const WeatherCard = ({data}) => {
-
+export  const WeatherCard = ({ data }) => {
+    // console.log("props data", data);
     const {current , location} = data;
-
     return (
-        <div className=' grid lg:grid-cols-4 md:grid-cols-2 gap-3 grid-cols-2 w-[90%] m-auto'>
-            <Card className=' border p-5'>
-                <h3 className=' text-center text-[1.2rem] font-medium'>Temperature</h3>
-                <p className=' text-center mt-4'>
-                    {current?.temp_c}°C
-                </p>
-            </Card>
-            <Card className=' border p-5'>
-                <h3 className=' text-center text-[1.2rem] font-medium'>Humidity</h3>
-                <p className=' text-center mt-4'>
-                    {current?.humidity}%
-                </p>
-            </Card>
-            <Card className=' border p-5'>
-                <h3 className=' text-center text-[1.2rem] font-medium'>Condition</h3>
-                <p className=' text-center mt-4'>
-                    {current?.condition?.text}
-                </p>
-            </Card>
-            <Card className=' border p-5'>
-                <h3 className=' text-center text-[1.2rem] font-medium'>Wind Speed</h3>
-                <p className=' text-center mt-4'>
-                    {current?.wind_kph}kph
-                </p>
-            </Card>
+        <div className="weather-cards">
+            <div className="weather-card">
+                <h1>Temperature</h1>
+                <p>{current?.temp_c}°C</p>
+            </div>
+            <div className="weather-card">
+                <h1>Humidity</h1>
+                <p>{current?.humidity}%</p>
+            </div>
+            <div className="weather-card">
+                <h1>Condition</h1>
+                <p>{current?.condition?.text}</p>
+            </div>
+            <div className="weather-card"> 
+                <h1>Wind Speed</h1>
+                <p>{current?.wind_kph}kph</p>
+            </div>
         </div>
     )
 }
+
